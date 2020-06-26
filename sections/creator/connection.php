7f9eed316e8e1c9d75bba87e8da056ec5f217bde
v3lli <emekaveli@gmail.com>
2020-06-26 02:22:26 +0100

    db settings

--- sections/creator/connection.php
+++ sections/creator/connection.php
@@ -1,11 +1,20 @@
-<?php
-$user = 'root';
-$password = 'root';
-$db = 'rvi';
-$host = 'localhost';
-$port = 8889;
-$connection = mysqli_connect($host,$user,$password,$db);
-if(mysqli_error($connection)){
-    echo 'No connection';
-}
+<!-- <?php
+// $user = 'root';
+// $password = 'root';
+// $db = 'rvi';
+// $host = 'localhost';
+// $port = 8889;
+// $connection = mysqli_connect($host,$user,$password,$db);
+// if(mysqli_error($connection)){
+//     echo 'No connection';
+// }
 
+ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+ 
+ $server = $url["host"];
+ $username = $url["user"];
+ $password = $url["pass"];
+ $db = substr($url["path"], 1);
+ 
+ $conn = new mysqli($server, $username, $password, $db);
+ ?>
